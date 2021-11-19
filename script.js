28a47589ec79709ee3f43c6ea2416f9021bb6f4c
MRomano26 <michael.mar26@gmail.com>
2021-11-19 15:08:59 -0500

    Create operator function that takes numbers and a operator to perform an equation

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 let add = (a, b) => a + b;
-
 let subtract = (a, b) => a - b;
-
 let multiply = (a, b) => a * b;
-
 let divide = (a, b) => a / b;
+
+function operate(a, b, operator) {
+    if (operator === "+") {
+        return add(a, b);
+    }
+    else if (operator === "-") {
+        return subtract(a, b);
+    }
+    else if (operator === "*") {
+        return multiply(a, b);
+    }
+    else if (operator === "/") {
+        return divide(a, b);
+    }
+    else {
+        return;
+    }
+}
