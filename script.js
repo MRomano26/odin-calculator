084901ec32134cd6900e7eb8d08c8f63310c2d42
MRomano26 <michael.mar26@gmail.com>
2021-11-19 17:29:33 -0500

    Number buttons decimal button and plus minus button now change the display

--- script.js
+++ script.js
@@ -20,3 +20,89 @@ function operate(a, b, operator) {
         return;
     }
 }
+
+
+// button elements
+const numbers = document.querySelectorAll(".number");
+const negative = document.querySelector("#NEG")
+const decimal = document.querySelector("#DECI")
+
+let displayValue = [""]
+
+
+// event listeners added
+numbers.forEach((number) => {
+    let id = number.id;
+    number.addEventListener("click", e => {
+        addNumberToDisplay(id);
+    })
+})
+
+negative.addEventListener("click", e => {
+    // changes sign on display
+    let signDisplay = document.querySelector(".signDisplay");
+    if (signDisplay.textContent === "") {
+        signDisplay.textContent = "-";
+    }
+    else {
+        signDisplay.textContent = "";
+    }
+    return displayValue[0] = signDisplay.textContent;
+})
+
+decimal.addEventListener("click", addDecimalToDisplay)
+
+
+// event listener functions
+function addNumberToDisplay(id) {
+    let displayNumbers = document.querySelectorAll(".numberDisplay");
+    displayNumbers = Array.from(displayNumbers);
+    for (let i = 0; i < displayNumbers.length; i++) {
+        if (displayNumbers[i].textContent !== "") {
+            continue;
+        }
+        else {
+            displayNumbers[i].textContent = `${id}`;
+            displayValue.push(displayNumbers[i].textContent)
+            return console.log(displayValue.join(""));
+        }
+    }
+    return console.log(displayValue.join(""))
+}
+
+function addDecimalToDisplay() {
+    let displayNumbers = document.querySelectorAll(".numberDisplay");
+    displayNumbers = Array.from(displayNumbers);
+
+    // fresh display condition
+    if (displayNumbers[0].textContent === "") {
+        return;
+    }
+
+    // when numbers are already on display
+    for (let i = 0; i < displayNumbers.length; i++) {
+        if (displayNumbers[i].textContent !== "") {
+            if (displayNumbers[i].textContent.includes(".")) {
+                if (displayNumbers[i + 1].textContent === "") {
+                    let lastNumber = displayNumbers[i].textContent.split("")[0];
+                    displayNumbers[i].textContent = lastNumber;
+                    displayValue.pop();
+                    displayValue.push(lastNumber);
+                    return console.log(displayValue.join("")); 
+                }
+                // nothing happens if decimal is in middle of value
+                return;
+            }
+            else {
+                continue;
+            }
+        }
+        else {
+            let lastNumber = displayNumbers[i - 1].textContent + ".";
+            displayNumbers[i - 1].textContent = lastNumber;
+            displayValue.pop();
+            displayValue.push(lastNumber);
+            return console.log(displayValue.join("")); 
+        }
+    }
+}
